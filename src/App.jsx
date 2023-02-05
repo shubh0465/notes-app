a6bb7f782e43f8f5690769ff225c42559beeac37
shubh0465 <55250438+shubh0465@users.noreply.github.com>
2023-02-05 10:05:30 +0530

    Rearrange the recently modified notes

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,29 @@ export default function App() {
     }
     
     function updateNote(text) {
-        setNotes(oldNotes => oldNotes.map(oldNote => {
-            return oldNote.id === currentNoteId
-                ? { ...oldNote, body: text }
-                : oldNote
-        }))
+        //This will not rearrange our notes
+        // setNotes(oldNotes => oldNotes.map(oldNote => {
+        //     return oldNote.id === currentNoteId
+        //         ? { ...oldNote, body: text }
+        //         : oldNote
+        // }))
+        //Try to rearrange recently modified notes
+        setNotes(
+            oldNotes =>
+            {
+                const newArray = []
+                for(let i=0; i<oldNotes.length; i++){
+                    const oldNote = oldNotes[i]
+                    if(oldNote.id === currentNoteId){
+                        newArray.unshift({...oldNote,body:text})
+                    }
+                    else{
+                        newArray.push(oldNote)
+                    }
+                }
+                return newArray
+            }
+        )
     }
     
     function findCurrentNote() {
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
 export default function Sidebar(props) {
-    const noteElements = props.notes.map((note, index) => (
-        <div key={note.id}>
+    const noteElements = props.notes.map((note) => {
+        const summary = note.body.split("\n")[0]
+        return <div key={note.id}>
             <div
 
                 className={`title ${note.id === props.currentNote.id ? "selected-note" : ""
                     }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">{summary}</h4>
             </div>
         </div>
-    ))
+    })
 
     return (
         <section className="pane sidebar">
